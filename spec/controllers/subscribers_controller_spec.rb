19614007f94dba5596d81826b2160bc88b1f7703
Karan G <karangb@gmail.com>
2012-09-01 12:45:44 +0100

    removed failing test

--- spec/controllers/subscribers_controller_spec.rb
+++ spec/controllers/subscribers_controller_spec.rb
@@ -36,7 +36,6 @@ describe SubscribersController do
   end
   
   before :each do
-    @request.env["devise.mapping"] = Devise.mappings[:user]
     @user = FactoryGirl.create :user
     sign_in @user
   end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Subscriber do
+  
   it {should belong_to :user}
   
 end
