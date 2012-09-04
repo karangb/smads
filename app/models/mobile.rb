a98f29584566b9b4e9d0bdee1541d043c75e94e4
Karan G <karangb@gmail.com>
2012-09-04 21:07:54 +0100

    updated mobile with rest call to tropo

--- app/models/mobile.rb
+++ app/models/mobile.rb
@@ -3,7 +3,7 @@ class Mobile
   base_uri 'api.tropo.com'
   
   def send_sms(number, message)
-    token = "155b98d0b352434ba4356c56dbf35bcbde5642b6916488d1749ac57b4f4da5415318522422bb0030250f78ca"
+    token = "155cfd2913673d4aac374e1e9ade2ea38e89662e5640034aa2e7db98e63b1f9052b57fab1f89f9e707bda8c7"
     self.class.get("/1.0/sessions?action=create&token=#{token}&numberToDial=#{number}&msg=#{message}")
   end
 end
