295ab0176dc53725cb6d1302ac867339c7294290
Karan G <karangb@gmail.com>
2012-09-04 20:10:13 +0100

    fixed test

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -4,11 +4,10 @@ describe Subscriber do
   
   it {should belong_to :user}
   
-  describe "send_message should use mobile to send message" do
+  it "send_message should use mobile to send message" do
     
-#     TODO: NEEDS FIXING
-    # subscriber = Subscriber.new(:number => "123")
-    # Mobile.any_instance.should_receive(:send_sms).with("123", "hello!")
-    # subscriber.send_message("hello!")
+    subscriber = Subscriber.new(:number => "123")
+    Mobile.any_instance.should_receive(:send_sms).with("123", "hello!")
+    subscriber.send_message("hello!")
   end
 end
